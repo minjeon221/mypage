569ad06b893a4e4fe859b45733588a935c262248
minjeon221 <peakmj94@gmail.com>
2021-06-01 21:24:15 +0900

    fullpage 첫파일

---
+++ fullpage/js/myfull.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('#myfull').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+    sectionsColor : ['#ccc', '#caca', '#acac', '#ddd'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips:['나의 소개', '포폴', '기타내용', '오렌지'],
+    showActiveTooltip: true
+	});
+
+
+})
